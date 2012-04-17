3ca192d239b73b4c33056a32999510e513685325
chris-twiner <chris.twiner@gmail.com>
2012-04-17 20:40:58 +0200

    attribute tests, qname equals factored out, will also factor out wether the qname shall be tested - could be the default, but probably not

--- core-tests/src/test/scala/scales/xml/EqualsNormalImportsTest.scala
+++ core-tests/src/test/scala/scales/xml/EqualsNormalImportsTest.scala
@@ -526,9 +526,31 @@ class EqualsNormalImportsTest extends junit.framework.TestCase {
   val x1 = po("root") /( po("child") ~> "po:value" )
   val x2 = npo("root") /( npo("child") ~> "npo:value" )
 
+  val x1_2 = po("root") /( po("child") ~> "po:value" )
+
   def testNormalQNames : Unit = {
     // the two documents are logically the same, but straight comparison will be different:
     assertFalse("x1 === x2 - before qname handling", x1 === x2)
+  }
+
+  def qnamesEqual(context : ComparisonContext, str : String, str2 : String) = {
+    // split both, if one has and the other not, then its false anyway
+    val sp1 = str.split(":")
+    val sp2 = str2.split(":")
+    if (sp1.size == 2 && sp2.size == 2) {
+      sp1(1) == sp2(1) && { // values match
+	// look up prefixes
+	(for{ lnc <- context.leftNamespaceContext
+	     rnc <- context.rightNamespaceContext
+	     lns <- lnc.mappings.get(sp1(0))
+	     rns <- rnc.mappings.get(sp2(0))
+	   } yield lns == rns
+       ).
+	getOrElse(false)
+      }
+    } else 
+      str == str2
+
   }
 
   // can be in a text or attribute node
@@ -539,24 +561,9 @@ class EqualsNormalImportsTest extends junit.framework.TestCase {
       def compare( calculate : Boolean , context : ComparisonContext, left : XmlItem, right : XmlItem) : Option[(XmlDifference[_], ComparisonContext)] = {
 	def check( str : String, str2 : String, doQNames : Boolean = false) = {
 	  val res = 
-	    if (doQNames) {
-	      // split both, if one has and the other not, then its false anyway
-	      val sp1 = str.split(":")
-	      val sp2 = str2.split(":")
-	      if (sp1.size == 2 && sp2.size == 2) {
-		sp1(1) == sp2(1) && { // values match
-		  // look up prefixes
-		  (for{ lnc <- context.leftNamespaceContext
-		      rnc <- context.rightNamespaceContext
-		       lns <- lnc.mappings.get(sp1(0))
-		       rns <- rnc.mappings.get(sp2(0))
-		     } yield lns == rns
-		  ).
-		  getOrElse(false)
-		}
-	      } else 
-		str == str2
-	    } else
+	    if (doQNames)
+	      qnamesEqual(context, str, str2)
+	    else
 	      str == str2
 
 	  if (res) None
@@ -583,8 +590,76 @@ class EqualsNormalImportsTest extends junit.framework.TestCase {
       }
     }
 
+    val noNS = po("root") /( po("child") ~> "pof:value" )
     
     assertTrue("x1 === x2 - qname is in", x1 === x2)
+    assertTrue("x1 === x1_2 ", x1 === x1_2)
+
+    // the prefix isn't there
+    assertFalse("x1 === noNS", x1 === noNS)
+    
+    // swap
+    assertFalse("noNS === x1", noNS === x1)
+    
+    // defined but different
+    val el = Elem(po("child")).copy( namespaces = Map( "pof" -> "uri:has" ))
+    val hasNS = po("root") /( el ~> "pof:value" )
+    assertFalse("x1 === hasNS", x1 === hasNS)
+    
+    // swap
+    assertFalse("hasNS === x1", hasNS === x1)
+    
+    // sanity
+    assertFalse("hasNS === noNS", hasNS === noNS)
+    assertFalse("noNS === hasNS", noNS === hasNS)
+    
+    // mixed up, taken from root
+    val m1 = npo("root") /( po("child") ~> "npo:value" )
+    val m2 = po("root") /( npo("child") ~> "po:value" )
+    assertTrue("m1 === m2 ", m1 === m2)
+
+    val m2_np = po("root") /( npo("child") ~> "value" )
+    assertFalse("m1 === m2_np ", m1 === m2_np)
   }
 
+
+/**
+ * Comparison between attributes, requires an Equal type class for QNames
+ */ 
+class AttributeComparisonQName(implicit eqn : Equal[QName]) extends XmlComparison[Attribute] {
+import SomeDifference._
+
+  import EqualsHelpers.toQName
+
+  def compare( calculate : Boolean, context : ComparisonContext, left: Attribute, right : Attribute) : Option[(XmlDifference[_], ComparisonContext)] = {
+    if (!eqn.equal(toQName(left.name), toQName(right.name)))
+      if (calculate)
+	Some((AttributeNameDifference( left, right), context))
+      else
+	noCalculation
+    else 
+      if (!qnamesEqual(context, left.value, right.value))
+	if (calculate)
+	  Some((AttributeValueDifference( left, right), context))
+	else
+	  noCalculation
+      else
+	None // a ok
+  }
+}
+  val ax1 = po("root") /( po("child") /@("loc" -> "po:value") )
+  val ax2 = npo("root") /( npo("child") /@("loc" -> "npo:value") )
+
+  def testNormalAttrQNames : Unit = {
+    assertFalse("ax1 === ax2", ax1 === ax2)
+  }
+
+  def testEmbeddedAttrQNames : Unit = {
+    /**
+     * any qname values are looked up
+     */ 
+    implicit def defaultAttributeComparison(implicit qe : Equal[QName]) : XmlComparison[Attribute] = new AttributeComparisonQName()(qe)
+
+    assertTrue("ax1 === ax2", ax1 === ax2)
+  }
 }
