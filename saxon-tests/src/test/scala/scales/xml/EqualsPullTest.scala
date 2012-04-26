3a2b4ce985bf014ac9c245510ae4332ea6783102
chris-twiner <chris.twiner@gmail.com>
2012-04-26 18:16:24 +0200

    convert remaining test, adding ref eq checks as well, let Pull disable them

--- saxon-tests/src/test/scala/scales/xml/EqualsPullTest.scala
+++ saxon-tests/src/test/scala/scales/xml/EqualsPullTest.scala
@@ -44,24 +44,33 @@ class EqualsPullTest extends EqualsNormalImportsTest {
     // swap the first out
     val c1 = m2.copy(prolog = m2.prolog.copy(misc = miscN))
 
-    assertFalse("m2 should not === c1", m2 === c1)
+    var pc1 = pull(c1)
+    var pm2 = pull(m2)
 
-    val countRes = compare(m2, c1)
+    assertFalse("pm2 should not === pc1", pm2 === pc1)
+
+    pc1 = pull(c1)
+    pm2 = pull(m2)
+
+    val countRes = compare(pm2, pc1)
     assertTrue("countRes should be defined", countRes.isDefined)
 
     val Some((DifferentNumberOfMiscs(cl,cr,true), ccontext)) = countRes
-    assertTrue("cl eq misc2", cl eq misc2)
-    assertTrue("cr eq miscN", cr eq miscN)
+    assertTrue("cl eq misc2", cl === misc2)
+    assertTrue("cr eq miscN", cr === miscN)
 
     // swap the end out
     val c2 = m2.copy(end = EndMisc(miscN))
+
+    val pc2 = pull(c2)
+    pm2 = pull(m2)
     
-    val ncountRes = compare(m2, c2)
+    val ncountRes = compare(pm2, pc2)
     assertTrue("ncountRes should be defined", ncountRes.isDefined)
 
     val Some((DifferentNumberOfMiscs(ncl,ncr,false), nccontext)) = ncountRes
-    assertTrue("ncl eq misc1", ncl eq misc1) // ends are different on purpose
-    assertTrue("ncr eq miscN", ncr eq miscN)
+    assertTrue("ncl eq misc1", ncl === misc1) // ends are different on purpose
+    assertTrue("ncr eq miscN", ncr === miscN)
   }
 
 
