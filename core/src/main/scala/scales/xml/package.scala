08caee118bc9a5e0de4e898c714bcb4df1513c57
chris-twiner <chris.twiner@gmail.com>
2012-07-29 11:00:35 +0200

    reproducable almost bare min

--- core/src/main/scala/scales/xml/package.scala
+++ core/src/main/scala/scales/xml/package.scala
@@ -1,32 +1,6 @@
 package scales
 
 package object xml extends XmlPulls
-// extends XmlTypes 
-//  with XmlParser 
-//  with XmlPaths
-//  with XPathMatcher 
-//  with XmlPrinter 
-//  with Whitespace 
-//  with XmlPulls 
-  with XmlFactories 
-//  with TraxSourceConversions
-//  with XmlUtils
   with PullIteratees
-//  with equals.XmlEquals 
 {
-/*
-  import strategies.ElemToken
-
-  val defaultPathOptimisation : PathOptimisationStrategy[QNameToken] = QNameMemoryOptimisation
-  val defaultOptimisation : MemoryOptimisationStrategy[QNameToken] = QNameMemoryOptimisation
-
-  val Functions = xpath.Functions
-
-  // work around my own version of SI-5031
-  @deprecated(message="Please import Functions._ instead - since 0.3")
-  val Attributes = xpath.Attributes
-  @deprecated(message="Please import Functions._ instead - since 0.3")
-  val Elements = xpath.Elements
-  @deprecated(message="Please import Functions._ instead - since 0.3")
-  val TextFunctions = xpath.OldTextFunctions */
 }
