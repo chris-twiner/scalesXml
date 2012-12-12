87a34b99ec16cfae343375713f698802c0ec13a7
chris-twiner <chris.twiner@gmail.com>
2012-12-12 10:33:00 +0100

    add package docs for impl

---
+++ core/src/main/scala/scales/xml/impl/package.scala
@@ -0,0 +1,7 @@
+package scales.xml
+
+/**
+ * This package is not intended for direct usage, however it provides much of the functionality that is used throughout scales.xml, and as such may be of use to those extending it.
+ */ 
+package object impl {
+}
