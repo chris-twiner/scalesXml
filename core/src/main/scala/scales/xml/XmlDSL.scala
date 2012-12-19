0d5e160d87d3bd4ccf3e776bef89313b3b3d3fbf
chris-twiner <chris.twiner@gmail.com>
2012-12-19 08:38:31 +0100

    docs and placeholder

--- core/src/main/scala/scales/xml/XmlDSL.scala
+++ core/src/main/scala/scales/xml/XmlDSL.scala
@@ -2,7 +2,31 @@ package scales.xml
 
 import ScalesXml.{xmlCBF, fromParserDefault} // note cannot be in parser here
 
+/**
+ * Creates DslBuilder instances
+ */
 object < {
+  /**
+   * Creates a DslBuilder with qname as the root Elems 
+   */ 
   def apply(qname : QName) = dsl.DslBuilder(Elem(qname))
+  /**
+   * Creates a DslBuilder with the given elem as root
+   */ 
   def apply(elem : Elem) = dsl.DslBuilder(elem)
 }
+
+/**
+ * Creates OptionalDslBuilder instances.  If none of the end Elems contain data (either attributes or child text nodes) the result of the builder is None.
+ *
+object ?< {
+  /**
+   * Creates an OptionalDslBuilder with qname as the root Elems 
+   */ 
+  def apply(qname : QName) = dsl.OptionalDslBuilder(Elem(qname))
+  /**
+   * Creates a OptionalDslBuilder with the given elem as root
+   */ 
+  def apply(elem : Elem) = dsl.OptionalDslBuilder(elem)
+}
+*/
